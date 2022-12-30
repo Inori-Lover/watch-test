9f7d4c884472e4d68d415c902c5d31fed6f4308a
inori_lover <inori_lover@outlook.com>
2022-12-30 23:09:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const state = ref({ deep: false })
+
+function onClick() {
+  state.value.deep = !state.value.deep
+}
+
+let count = 0
+
+watch(() => [state.value.deep], () => {
+  state.value = { deep: false }
+  console.log('watch', ++count)
+})
+</script>
+
+<template>
+  <button @click="onClick">click: {{ state.deep }}</button>
+</template>
